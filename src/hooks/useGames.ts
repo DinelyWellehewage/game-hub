4dbc58e3ed9918a8b997ce4c0a1494996cb2aeda
DinelyWellehewage <dinelywh78@gmail.com>
2024-07-11 12:39:51 +0200

    implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,6 +32,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
