8037d74aec22a1530d5efde525badfab6a5b0a2f
DinelyWellehewage <dinelywh78@gmail.com>
2024-07-11 06:24:47 +0200

    displaying the genres

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg"></Show>
-      <GridItem area="aside">
+      <GridItem area="aside" padding={5}>
         <GenreList />
       </GridItem>
       <GridItem area="main">
